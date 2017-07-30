287f32cffa7629e71518e1f10e549f8e61ec52b3
TatsushiToji <tatsushi.toji@gmail.com>
2017-07-30 23:51:03 +0900

    :tada: initial commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+
+export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>
+              <Icon name='logo-apple' />
+            </Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Right>
+        </Header>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+## My First CRNA
+
+dependencies
+
+* [CRNA](https://github.com/react-community/create-react-native-app)
+* [Native-Base](https://github.com/GeekyAnts/NativeBase)
